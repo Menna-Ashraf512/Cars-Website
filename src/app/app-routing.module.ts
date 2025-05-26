699fef98ad19062a22d0b2d6c6d0524628e1f635
Menna-Ashraf512 <mennaalla@gmail.com>
2025-05-27 01:53:57 +0300

    Project preparation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/pages/home/home.component';
+import { AllCarsComponent } from './core/pages/all-cars/all-cars.component';
+import { CarDetailsComponent } from './core/pages/car-details/car-details.component';
+import { NotFoundComponent } from './shared/component/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home', pathMatch:"full"},
+  {path:'home',component:HomeComponent, title:"home page"},
+  {path:'allCars',component:AllCarsComponent, title:"Cars page"},
+  {path:'carDetails',component:CarDetailsComponent, title:"CarDetails page"},
+  {path:'**',component:NotFoundComponent, title:"404 page"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/pages/home/home.component';
+import { AllCarsComponent } from './core/pages/all-cars/all-cars.component';
+import { CarDetailsComponent } from './core/pages/car-details/car-details.component';
+import { NavbarComponent } from './core/layout/navbar/navbar.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
+import { NotFoundComponent } from './shared/component/not-found/not-found.component';
+import { SignInComponent } from './core/pages/auth/sign-in/sign-in.component';
+import { SignOutComponent } from './core/pages/auth/sign-out/sign-out.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AllCarsComponent,
+    CarDetailsComponent,
+    NavbarComponent,
+    FooterComponent,
+    NotFoundComponent,
+    SignInComponent,
+    SignOutComponent
   ],
   imports: [
     BrowserModule,
