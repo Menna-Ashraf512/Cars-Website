e9b5e57c716b6ccea465a35183574108863cb878
Menna-Ashraf512 <mennaalla@gmail.com>
2025-05-27 07:07:43 +0300

    add HttpClientModule to app.module and create CarsService for API interactions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './core/pages/home/home.component';
@@ -30,7 +30,8 @@ import { PopularCarsComponent } from './core/component/popular-cars/popular-cars
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/services/cars.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarsService {
+
+  constructor(private _HttpClient:HttpClient) { }
+    getAllCars():Observable<any>{
+    return this._HttpClient.get('https://myfakeapi.com/api/cars/')
+  }
+  getPostCarId(id:number):Observable<any>{
+    return this._HttpClient.get(`https://myfakeapi.com/api/cars/${id}`)
+  }
+}
